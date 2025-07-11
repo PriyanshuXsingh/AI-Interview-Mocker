aba5df25590e467d25e2556666088ed7e108e49d
PriyanshuXsingh <162482010+PriyanshuXsingh@users.noreply.github.com>
2025-07-11 08:56:58 +0530

    add

--- app/dashboard/_components/InterviewList.jsx
+++ app/dashboard/_components/InterviewList.jsx
@@ -6,34 +6,51 @@ import { MockInterview } from "@/utils/schema";
 import { eq } from "drizzle-orm";
 import { desc } from "drizzle-orm";
 import InterviewItemCard from "./InterviewItemCard";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 const InterviewList = () => {
   const { user } = useUser();
   const [interviewList, setInterviewList] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
 useEffect(() => {
 user&&GetInterviewList();
 }, [user]);
 
   const GetInterviewList = async () => {
-    const result = await db
-      .select()
-      .from(MockInterview)
-      .where(
-        eq(MockInterview.createdBy, user?.primaryEmailAddress?.emailAddress)
-      )
-      .orderBy(desc(MockInterview.id));
-    console.log("Interview List:", result);
-    setInterviewList(result);
+       try {
+      setLoading(true);
+      const result = await db
+        .select()
+        .from(MockInterview)
+        .where(
+          eq(MockInterview.createdBy, user?.primaryEmailAddress?.emailAddress)
+        )
+        .orderBy(desc(MockInterview.id));
+
+      setInterviewList(result);
+    } catch (error) {
+      console.error("Failed to fetch interviews:", error);
+    } finally {
+      setLoading(false);
+    }
     
   };
   return (
     <div>
       <h2 className="font-medium text-xl">Previous Mock Interview</h2>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5 '>
-        {interviewList&&interviewList.map((interview,index)=>(
-            <InterviewItemCard
-            interview={interview}
-            key={index}/>
-        ))}
+        {loading
+          ? Array.from({ length: 6 }).map((_, i) => (
+              <div key={i} className="p-4 border rounded-xl shadow-md">
+                <Skeleton height={20} width={"80%"} />
+                <Skeleton height={15} width={"60%"} className="mt-2" />
+                <Skeleton height={100} className="mt-4" />
+              </div>
+            ))
+          : interviewList.map((interview, index) => (
+              <InterviewItemCard interview={interview} key={index} />
+            ))}
       </div>
     </div>
   );
