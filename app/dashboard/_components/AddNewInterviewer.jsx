896484f96c9567b966d8a3edc18b3d96ad3ef631
PriyanshuXsingh <162482010+PriyanshuXsingh@users.noreply.github.com>
2025-08-03 13:04:39 +0530

    close icon working

--- app/dashboard/_components/AddNewInterviewer.jsx
+++ app/dashboard/_components/AddNewInterviewer.jsx
@@ -89,7 +89,7 @@ function AddNewInterviewer() {
       >
         <h2 className=" text-lg text-center">+Add New</h2>
       </div>
-      <Dialog open={openDialog}>
+      <Dialog open={openDialog} onOpenChange={setOpenDialog}>
         <DialogContent className="max-w-2xl">
           <DialogHeader>
             <DialogTitle className="text-2xl">
